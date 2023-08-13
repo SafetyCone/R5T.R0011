276fb11247960ce6993adf895d5ff6e14b94917e
davidcoats <david@getsafetycone.com>
2023-08-12 23:23:32 -0700

    Interim changes.

---
+++ source/R5T.R0011.Client/Components/Layout/Layout.razor.cs
@@ -0,0 +1,16 @@
+using System;
+
+using R5T.T0154;
+
+
+namespace R5T.R0011.Client.Layouts
+{
+    /// <summary>
+    /// Default layout.
+    /// </summary>
+    [RazorLayoutMarker]
+    public partial class Layout
+    {
+
+    }
+}
---
+++ source/R5T.R0011/Components/CssOutTailwindStylesheetLink.razor.cs
@@ -0,0 +1,16 @@
+using System;
+
+using R5T.T0154;
+
+
+namespace R5T.R0011.Components
+{
+    /// <summary>
+    /// Adds a link to the Tailwind CSS stylesheet conventionally placed at "css-out/tailwind.css".
+    /// </summary>
+    [RazorComponentMarker]
+    public partial class CssOutTailwindStylesheetLink : IRazorComponentMarker
+    {
+
+    }
+}
